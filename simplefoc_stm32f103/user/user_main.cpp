10b7bce5a934134c3fe0a6ecf564ef3686a96bb3
wallacezhang <wallace.zhang@fjdynamics.com>
2024-12-25 20:01:55 +0800

    1、完成新笔记。

--- simplefoc_stm32f103/user/user_main.cpp
+++ simplefoc_stm32f103/user/user_main.cpp
@@ -8,7 +8,7 @@ void main_Cpp(void)
 {
     while(1) {
         HAL_GPIO_TogglePin(run_led_GPIO_Port,run_led_Pin); // 心跳灯跑起来
-        SEGGER_RTT_printf(0,"Dwt_time_us:%d\n",_micros()); // 打印DWT定时器的us级时间戳，看看有没有问题
+        SEGGER_RTT_printf(0,"Dwt_time_us:%u\n",_micros()); // 打印DWT定时器的us级时间戳，看看有没有问题
         delayMicroseconds(100000U); // 延时100ms
     }
 }
