bbca15204b41d709e316ae3a2644ab661c855b2b
wallacezhang <wallace.zhang@fjdynamics.com>
2024-12-25 20:05:06 +0800

    1、增加新笔记。

---
+++ simplefoc02_stm32f103/user/user_main.cpp
@@ -0,0 +1,25 @@
+#include "user_main.h"
+
+/**
+ * @brief C++环境入口函数
+ * 
+ */
+void main_Cpp(void)
+{
+    while(1) {
+        HAL_GPIO_TogglePin(run_led_GPIO_Port,run_led_Pin); // 心跳灯跑起来
+        SEGGER_RTT_printf(0,"Dwt_time_us:%u\n",_micros()); // 打印DWT定时器的us级时间戳，看看有没有问题
+        delayMicroseconds(100000U); // 延时100ms
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
