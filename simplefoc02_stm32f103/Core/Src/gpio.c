cf5b2ef7e371421e9cc1149642be169bcefd6ede
wallacezhang <wallace.zhang@fjdynamics.com>
2024-12-27 14:03:27 +0800

    1、新例程。

--- simplefoc02_stm32f103/Core/Src/gpio.c
+++ simplefoc02_stm32f103/Core/Src/gpio.c
@@ -51,14 +51,14 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(run_led_GPIO_Port, run_led_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = run_led_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(run_led_GPIO_Port, &GPIO_InitStruct);
 
 }
 
---
+++ simplefoc04_stm32f103_open_vel_ctrl/user/user_main.cpp
@@ -0,0 +1,46 @@
+#include "user_main.h"
+#include "AS5600_I2C.h"
+
+// 外部变量
+extern struct AS5600_I2CConfig_s AS5600_I2C_Config;
+extern I2C_HandleTypeDef hi2c1;
+
+// 全局变量
+AS5600_I2C AS5600_1(AS5600_I2C_Config); // 创建AS5600_I2C对象
+float g_Velocity; // 便于使用J-LINK Scope观察曲线
+
+
+void SEGGER_Printf_Float(float value){
+    char buffer[50];
+    sprintf(buffer, "%.4f\n", value); // 格式化为字符串
+    SEGGER_RTT_printf(0, "motor_velocity:%s", buffer); // 打印字符串
+}
+
+/**
+ * @brief C++环境入口函数
+ * 
+ */
+void main_Cpp(void)
+{
+    AS5600_1.init(&hi2c1); // 初始化AS5600
+    HAL_Delay(1000); // 延时1s
+    while(1) {
+        HAL_GPIO_TogglePin(run_led_GPIO_Port,run_led_Pin); // 心跳灯跑起来
+        //SEGGER_RTT_printf(0,"Dwt_time_us:%u\n",_micros()); // 打印DWT定时器的us级时间戳，看看有没有问题
+        AS5600_1.update(); // 更新位置，获取速度
+        g_Velocity = AS5600_1.getVelocity(); // 获取速度
+        SEGGER_Printf_Float(g_Velocity); // 打印电机速度
+        delayMicroseconds(10000U); // 延时10ms
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
