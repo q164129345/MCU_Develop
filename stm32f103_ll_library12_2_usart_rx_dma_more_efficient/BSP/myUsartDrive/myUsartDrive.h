e80aeb6a73d46d570c716f9ab14ddff97c9779ff
wallacezhang <wallace.zhang@fjdynamics.com>
2025-05-20 14:50:01 +0800

    1、增加串口新章节。

---
+++ stm32f103_ll_library12_2_usart_rx_dma_more_efficient/BSP/myUsartDrive/myUsartDrive.h
@@ -0,0 +1,83 @@
+/**
+ * @file myUsartDrive.h
+ * @brief Low-layer driver interface definitions for USART1 module
+ * 
+ * This header file contains function declarations, buffer definitions, and 
+ * low-level control interfaces for USART1 using STM32 LL (Low-Layer) library.
+ * 
+ * @note This file is intended for low-level register access and control of USART1 
+ *       peripherals, based on the STM32 LL library.
+ * 
+ * @version 1.0.0
+ * @date 2025-04-29
+ * @author Wallace.zhang
+ * 
+ * @copyright
+ * (C) 2025 Wallace.zhang. All rights reserved.
+ * 
+ * @license SPDX-License-Identifier: MIT
+ */
+#ifndef __MYUSARTDRIVE_H
+#define __MYUSARTDRIVE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+/** 
+ * @brief USART RX buffer size definition (unit: bytes)
+ */
+#define RX_BUFFER_SIZE 1024U
+
+/** 
+ * @brief USART TX buffer size definition (unit: bytes)
+ */
+#define TX_BUFFER_SIZE 2048U
+
+/**
+  * @brief  阻塞方式发送字符串
+  */
+void USART1_SendString_Blocking(const char* str);
+
+/**
+  * @brief   将数据写入 USART1 发送 ringbuffer 中
+  */
+uint8_t USART1_Put_TxData_To_Ringbuffer(const void* data, uint16_t len);
+
+/**
+ * @brief Reinitialize USART1 receiving DMA
+ */
+void USART1_Reinit(void);
+
+/**
+ * @brief DMA1_Channel4 interrupt handler for USART1 TX
+ */
+void USART1_TX_DMA1_Channel4_Interrupt_Handler(void);
+
+/**
+ * @brief DMA1_Channel5 interrupt handler for USART1 RX
+ */
+void USART1_RX_DMA1_Channel5_Interrupt_Handler(void);
+
+/**
+ * @brief USART1 global interrupt handler (RX idle detection, error handling)
+ */
+void USART1_RX_Interrupt_Handler(void);
+
+/**
+ * @brief USART1 main module run function (should be called in main loop)
+ */
+void USART1_Module_Run(void);
+
+/**
+ * @brief Configure USART1 peripheral and DMA channels
+ */
+void USART1_Config(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MYUSARTDRIVE_H */
---
+++ stm32f103_ll_library12_2_usart_rx_dma_more_efficient/MDK-ARM/stm32f103zet6/mycandrive_reg.d
@@ -0,0 +1,27 @@
+stm32f103zet6/mycandrive_reg.o: ..\BSP\myCanDrive\myCanDrive_reg.c \
+  ..\BSP\myCanDrive\myCanDrive_reg.h ..\Core\Inc\main.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_dma.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f1xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f103xe.h \
+  ..\Drivers\CMSIS\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  D:\Coding\MCU_Develop\stm32f103_ll_library12_2_usart_rx_dma_more_efficient\Drivers\CMSIS\Include\cmsis_version.h \
+  D:\Coding\MCU_Develop\stm32f103_ll_library12_2_usart_rx_dma_more_efficient\Drivers\CMSIS\Include\cmsis_compiler.h \
+  D:\Coding\MCU_Develop\stm32f103_ll_library12_2_usart_rx_dma_more_efficient\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\system_stm32f1xx.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_rcc.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_bus.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_system.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_exti.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_cortex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_utils.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_pwr.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_usart.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_ll_gpio.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdbool.h ..\BSP\lwrb\lwrb.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdatomic.h
