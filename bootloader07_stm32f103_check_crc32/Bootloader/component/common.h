1b6b931d36220060080fd69042fe0b7852e937f3
wallacezhang <wallace.zhang@fjdynamics.com>
2025-05-29 14:03:19 +0800

    1、修改名字。

---
+++ bootloader07_stm32f103_check_crc32/Bootloader/component/common.h
@@ -0,0 +1,18 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+
+void Delay_MS_By_NOP(uint32_t ms);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
