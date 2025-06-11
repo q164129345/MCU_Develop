c627095caa3334f0fb9d3c8158a42b519d388bd0
wallacezhang <wallace.zhang@fjdynamics.com>
2025-06-11 09:23:06 +0800

    1、增加注释。

--- bootloader09_stm32f103_iap_protocol/iap_py/serial_manager.py
+++ bootloader09_stm32f103_iap_protocol/iap_py/serial_manager.py
@@ -7,15 +7,33 @@ class SerialManager:
             baudrate 波特率
     """
     def __init__(self, port, baudrate):
+        """
+        Brief: 初始化串口
+        Params: port - 串口号，如COM3
+                baudrate - 波特率
+        """
         self.ser = serial.Serial(port, baudrate, timeout=1)
 
     def Send_String(self, data):
-        self.ser.write(data.encode('utf-8'))
+        """
+        Brief: 发送字符串数据
+        Params: data - 要发送的字符串
+        Return: 实际发送的字节数
+        """
+        return self.ser.write(data.encode('utf-8'))
     
     def Send_Bytes(self, data):
-        self.ser.write(data)
+        """
+        Brief: 发送字节数据
+        Params: data - 要发送的bytes对象
+        Return: 实际发送的字节数
+        """
+        return self.ser.write(data)
 
     def close(self):
+        """
+        Brief: 关闭串口
+        """
         self.ser.close()
 
 
