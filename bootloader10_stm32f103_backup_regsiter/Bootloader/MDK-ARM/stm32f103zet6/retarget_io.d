932106d363f26c025f122e97862a7b414f7d1318
wallacezhang <wallace.zhang@fjdynamics.com>
2025-06-26 19:25:29 +0800

    1、删除bootloader第10章节。

--- bootloader10_stm32f103_backup_regsiter/Bootloader/MDK-ARM/stm32f103zet6/retarget_io.d
+++
@@ -1,8 +0,0 @@
-stm32f103zet6/retarget_io.o: ..\EventRecorder\retarget_io.c \
-  C:\Keil_v5\ARM\ARMCLANG\include\string.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\assert.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\rt_sys.h \
-  RTE\_stm32f103zet6\RTE_Components.h
--- bootloader10_stm32f103_backup_regsiter/Bootloader/MDK-ARM/stm32f103zet6/soft_crc32.d
+++
@@ -1,3 +0,0 @@
-stm32f103zet6/soft_crc32.o: ..\IAP\soft_crc32.c ..\IAP\soft_crc32.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\string.h
